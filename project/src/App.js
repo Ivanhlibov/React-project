fc2dee284990d1d143ce3c3c0738524111b202f7
Ivan_Hlibov <92669197+Ivanhlibov@users.noreply.github.com>
2024-10-05 12:32:41 +0300

    start project

---
+++ project/src/App.js
@@ -0,0 +1,13 @@
+import style from './style.module.css'
+
+function App() {
+
+  return (
+    <div className="App">
+    <h1 className={style.text} >Hello</h1>
+    </div>
+    
+  );
+}
+
+export default App;
