0475ea1015d8b57c11d0426af80a90414462bedc
Ivan_Hlibov <92669197+Ivanhlibov@users.noreply.github.com>
2024-10-05 13:27:23 +0300

    end project

--- project/src/App.js
+++ project/src/App.js
@@ -1,13 +1,24 @@
-import style from './style.module.css'
+import React from 'react';
 
-function App() {
+import { Footer, Blog, Possibility, Features, WhatGPT3, Header } from './containers';
+import { CTA, Brand, Navbar } from './components';
 
-  return (
-    <div className="App">
-    <h1 className={style.text} >Hello</h1>
+import './App.css';
+
+const App = () => (
+  <div className="App">
+    <div className="gradient__bg">
+      <Navbar />
+      <Header />
     </div>
-    
-  );
-}
+    <Brand />
+    <WhatGPT3 />
+    <Features />
+    <Possibility />
+    <CTA />
+    <Blog />
+    <Footer />
+  </div>
+);
 
 export default App;
